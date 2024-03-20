86ec1c8e82e7ddc3f75400f1e05c4c0651b75047
Phellipe <phellipelisboa.dev@gmail.com>
2024-03-20 06:57:19 -0300

    register/login icon

--- views/session/create.view.php
+++ views/session/create.view.php
@@ -6,7 +6,7 @@
 
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            <img class="mx-auto h-10 w-auto" src="https://openclipart.org/download/247319/abstract-user-flat-3.svg"
                 alt="Your Company">
             <h2 class="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Login</h2>
         </div>
