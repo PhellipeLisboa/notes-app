5fe052f2e2436da5259e6c5bb846cde66f5ef6c0
Phellipe Lisboa <phellipelisboa.dev@gmail.com>
2024-03-21 09:40:32 -0300

    Home page redirects to register or note create if logged in

--- views/index.view.php
+++ views/index.view.php
@@ -2,10 +2,21 @@
 <?php require('partials/nav.php'); ?>
 <?php require('partials/banner.php'); ?>
 
-<main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <p>Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?></p>
+
+
+<div class="flex flex-col justify-around mb-2 px-5 py-4 bg-neutral-200 rounded-lg shadow-inner min-h-[625px]">
+
+    <div class="mx-auto max-w-7xl  sm:px-6 lg:px-8">
+        <h2 class="text-3xl font-bold drop-shadow-lg">Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?></h2>
     </div>
-</main>
+
+    <a href="<?= empty($_SESSION) ? '/register-for-note' : '/notes/create' ?>"
+        class="bg-indigo-900 mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 hover:text-blue-700 mt-5 rounded-lg shadow-lg text-center  text-white font-lg font-bold hover:underline">
+        Start taking notes
+    </a>
+
+</div>
+
+
 
 <?php require('partials/footer.php')?>
